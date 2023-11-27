d7a1a5638a84371492f787681de9dd869026502a
Daniel Qian <daniel.qian@nyu.edu>
2023-11-27 17:43:21 -0500

    update docs

--- src/seg_model.py
+++ src/seg_model.py
@@ -4,8 +4,4 @@ import torch.nn as nn
 
 
 class SegmentationModel(nn.Module):
-    def __init__(self, siam, dim=2048, pred_dim=512):
-        self.encoder = siam.encoder
-        self.predictor = siam.predictor
-
-        self.decoder = 
+    pass
